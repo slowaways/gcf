b535353ff9a41c0dbfff5616ea9983cb602164ed
Rodrigo Vieira <rodrigo@vieira.com>
2019-02-01 00:35:17 -0300

    Register only when loading the Gutenberg page
    * Compatible with Wordpress Gutenberg plugin (Wp < 5.0)

--- lib/register-templates.php
+++ lib/register-templates.php
@@ -1,41 +1,50 @@
 <?php
-
 /**
- * TODO: Do this only when loading the Gutenberg page
  *
  * Register the gutenberg custom templates
  */
 function register_gutenberg_custom_templates() {
-	$templates = get_posts( array( 'post_type' => 'gcf-template') );
+    // Wordpress 5.0+
+    $current_screen = get_current_screen();
+    $is_block_editor = method_exists($current_screen, 'is_block_editor') 
+                       && $current_screen->is_block_editor();
+    // Gutenberg Plugin (Wordpress < 5.0)
+    $is_gutenberg_page = function_exists('is_gutenberg_page') && is_gutenberg_page();
 
-	$collection = array();
-	foreach ( $templates as $template ) {
-		$post_type = get_post_type_object( get_post_meta( $template->ID, 'post_type', true ) );
-		if ( $post_type ) {
-			// Computing the template.
-			$gutenberg_template = array();
-			$fields_config = json_decode( get_post_meta( $template->ID, 'fields', true ) );
-			foreach( $fields_config as $field_config ) {
-				$gutenberg_template[] = array(
-					sprintf( 'gcf/gcf-%s', $field_config->id )
-				);
-				register_meta( 'post', $field_config->name, array(
-					'show_in_rest' => true,
-					'single' => true,
-					'type' => 'string',
-				) );
-			}
-			$post_type->template = $gutenberg_template;
+    if ( $is_block_editor or $is_gutenberg_page ) {
+        $templates = get_posts( array( 'post_type' => 'gcf-template') );
 
-			// Computing the lock config.
-			$lock = get_post_meta( $template->ID, 'lock', true );
-			if ( $lock && $lock !== 'none' ) {
-				$post_type->template_lock = $lock;
-			}
-		}
-	}
+        $collection = array();
+        foreach ( $templates as $template ) {
+            $post_type = get_post_type_object( get_post_meta( $template->ID, 'post_type', true ) );
+            if ( $post_type ) {
+                // Computing the template.
+                $gutenberg_template = array();
+                $fields_config = json_decode( get_post_meta( $template->ID, 'fields', true ) );
+
+                foreach( $fields_config as $field_config ) {
+                    $gutenberg_template[] = array(
+                        sprintf( 'gcf/gcf-%s', $field_config->id )
+                    );
+                    register_meta( 'post', $field_config->name, array(
+                        'show_in_rest' => true,
+                        'single' => true,
+                        'type' => 'string',
+                    ) );
+                }
+                new \Debug\Log($gutenberg_template);
+                $post_type->template = $gutenberg_template;
+
+                // Computing the lock config.
+                $lock = get_post_meta( $template->ID, 'lock', true );
+                if ( $lock && $lock !== 'none' ) {
+                    $post_type->template_lock = $lock;
+                }
+            }
+        }
+    }
 }
-add_action( 'init', 'register_gutenberg_custom_templates' );
+add_action('admin_enqueue_scripts', 'register_gutenberg_custom_templates');
 
 /**
  * TODO: Limit to the currently used template
